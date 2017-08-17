cb7d564bfb0a8dada1b1aea990b0c19b1e11dd29
Jeff Irwin <jeff.irwin@live.com>
2017-08-17 11:24:59 -0500

    updated configs

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 require('dotenv').config();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect(`${process.env.MONGODB_URI}`, { useMongoClient: true });
+
+const Person = require('./models/person');
 
 const app = express();
 
-const mustache = mustacheExpress()
-mustache.cache = null;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const mustache = mustacheExpress();
+if (process.env.NODE_ENV ==='development') {
+  mustache.cache = null;
+}
 app.engine('mustache', mustache);
 app.set('view engine', 'mustache');
 
@@ -17,6 +27,22 @@ app.get('/list', (req, res) => {
 
 app.get('/book-form', (req, res) => {
   res.render('book-form');
+});
+
+app.get('/people', (req, res) => {
+  Person.find({}, (err, people) => {
+    res.json(people);
+  });
+});
+
+app.post('/addperson', (req, res) => {
+  const person = new Person();
+  person.firstName = req.body.firstName;
+  person.lastName = req.body.lastName;
+  person.save((err, person) => {
+    console.log('I created', person);
+    res.redirect('/');
+  });
 });
 
 app.listen(process.env.PORT, () => {
